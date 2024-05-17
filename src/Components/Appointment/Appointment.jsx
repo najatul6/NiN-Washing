80b8dfb3de0ea5426d58221858470a2f671c90ac
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-05-17 06:32:09 +0600

    Create apointment section

---
+++ src/Components/Appointment/Appointment.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import Button from "../Shared/Button/Button";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { RiInstagramFill } from "react-icons/ri";
+import { TiSocialYoutubeCircular } from "react-icons/ti";
+
+const Appointment = () => {
+  return (
+    <div className="bg-deep-blue space-y-5 text-white">
+      <h4 className="text-lg font-bold">Get Started</h4>
+      <h2 className="text-2xl font-bold">Book Your Appointment</h2>
+      <p className="text-white-coffee">
+        Ready to get started? Call us today or fill out our form for a fast, no
+        hassle, no pressure cleaning quote.
+      </p>
+
+      {/* Social Links  */}
+      <div className="flex justify-center items-center gap-6">
+        <Link to="https://www.facebook.com/najatul6/" target="_blank">
+          <Button
+            extraClass="bg-white-coffee border-2 hover:bg-transparent p-2 rounded-xl text-white"
+            afterIcn={<FaFacebook />}
+          />{" "}
+        </Link>
+        <Link to="https://twitter.com/MNajatul" target="_blank">
+          <Button
+            extraClass="bg-white-coffee border-2 hover:bg-transparent p-2 rounded-xl text-white"
+            afterIcn={<AiFillTwitterCircle />}
+          />{" "}
+        </Link>
+        <Link to="https://www.instagram.com/najatulislam1/" target="_blank">
+          <Button
+            extraClass="bg-white-coffee border-2 hover:bg-transparent p-2 rounded-xl text-white"
+            afterIcn={<RiInstagramFill />}
+          />{" "}
+        </Link>
+        <Link to="https://www.youtube.com/@mdnajatulislam8347/featured" target="_blank">
+          <Button
+            extraClass="bg-white-coffee border-2 hover:bg-transparent p-2 rounded-xl text-white"
+            afterIcn={<TiSocialYoutubeCircular />}
+          />{" "}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Appointment;
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,14 +1,19 @@
 import { Helmet } from "react-helmet-async";
+import PageTitleSection from "../../Components/Shared/PageTitleSection/PageTitleSection";
 
 const Contact = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>NiN Washing | Contact</title>
-            </Helmet>
-            Contact is hereby Contact
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>NiN Washing | Contact</title>
+      </Helmet>
+      <PageTitleSection
+        title="Contact Us"
+        subTitle="Need to book an appointment or an emergency help? Get in touch with us now and our representative will help you soon."
+      />
+      <div></div>
+    </div>
+  );
 };
 
 export default Contact;
