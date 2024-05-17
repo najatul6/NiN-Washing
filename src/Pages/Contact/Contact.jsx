005e300f65e2da44db5f286cc9faa41ce5a5dbd2
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-05-18 02:51:36 +0600

    all loading effect show

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -40,6 +40,3 @@ const Contact = () => {
 export default Contact;
 
 
-{/* <div className="flex justify-center items-center h-screen">
-          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-200"></div>
-        </div> */}
