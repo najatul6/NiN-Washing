c88d1a89dc6cd020052eb6def4988bc38e311c4e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-05-17 06:13:04 +0600

    complete service page design

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,15 +1,25 @@
 import { Helmet } from "react-helmet-async";
 import ServicesCard from "../../Components/ServicesCard/ServicesCard";
+import PageTitleSection from "../../Components/Shared/PageTitleSection/PageTitleSection";
 
 const Services = () => {
-    return (
-        <div className="p-5">
-            <Helmet>
-                <title>NiN Washing | Services</title>
-            </Helmet>
-            <ServicesCard/>
-        </div>
-    );
+  return (
+    <div className="">
+      <Helmet>
+        <title>NiN Washing | Services</title>
+      </Helmet>
+      <PageTitleSection
+        title="Our Services"
+        subTitle="We pride ourselves on being able to handle all types of exterior cleaning projects for both residential and commercial properties."
+      />
+      <div className="p-2 md:p-10">
+        <h2 className="text-deep-blue font-bold text-xl md:text-xl lg:text-3xl my-5">
+          What We Offer
+        </h2>
+        <ServicesCard />
+      </div>
+    </div>
+  );
 };
 
 export default Services;
